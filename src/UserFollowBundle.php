4e6e017da418ff5f499c8bdde784ed92b8e9aea8
github-ci <you@example.com>
2025-11-19 22:18:30 +0800

    update

--- src/UserFollowBundle.php
+++ src/UserFollowBundle.php
@@ -5,6 +5,10 @@ namespace Tourze\UserFollowBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle;
+use Tourze\DoctrineSnowflakeBundle\DoctrineSnowflakeBundle;
+use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
+use Tourze\DoctrineUserBundle\DoctrineUserBundle;
 use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class UserFollowBundle extends Bundle implements BundleDependencyInterface
@@ -14,6 +18,10 @@ class UserFollowBundle extends Bundle implements BundleDependencyInterface
         return [
             DoctrineBundle::class => ['all' => true],
             EasyAdminMenuBundle::class => ['all' => true],
+            DoctrineIndexedBundle::class => ['all' => true],
+            DoctrineSnowflakeBundle::class => ['all' => true],
+            DoctrineTimestampBundle::class => ['all' => true],
+            DoctrineUserBundle::class => ['all' => true],
         ];
     }
 }
